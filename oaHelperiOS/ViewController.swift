9951f0f548c956c0fee13f3324a42dcd6bee7145
Claus Wolf <claus@mactopics.de>
2018-12-08 18:08:03 +0100

    Initial Commit

---
+++ oaHelperiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  oaHelperiOS
+//
+//  Created by Claus Wolf on 08.12.18.
+//  Copyright © 2018 Claus Wolf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
